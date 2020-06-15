f3f399557db7e3edef85b84b049196e51eac4cc1
Stephen Crispin Mwanika <45911158+scmwanika@users.noreply.github.com>
2020-06-16 01:17:22 +0300

    commenting_exercise

---
+++ exercise.js
@@ -0,0 +1,51 @@
+// Create a variable called name.
+var name;
+// Assign a string "ozzy queen" to name.
+name = " ozzy queen"
+// Output name on the console.
+console.log(name);
+// Output the string "<br/>" on the console.
+console.log("<br/>");
+// Create another variable called name.
+var name;
+// Assign a string "ozzy hum" to name.
+name = "ozzy hum"
+// Output name on the console.
+console.log(name)
+// Create another variable called name.
+var name;
+// Assign a string "sumi mike ozyy" to name.
+name = "sumi mike ozyy"
+// Output name on the console.
+console.log(name)
+// Create another variable called name.
+var name;
+// Assign a string "ozzy bp" to name.
+name = " ozzy bp"
+// Output name on the console.
+console.log(name)
+// Assign a string "Bridget ozzy" to name.
+name = " Bridget ozzy"
+// Output name on the console.
+console.log(name)
+// Create a variable called age.
+var age;
+// Create another variable called name.
+var name;
+/* 
+Assign an integer 50 to age.
+Assign a string "mirembe agaba kwesi" to name.
+*/
+age = 50;
+name = "mirembe agaba kwesi"
+// Output the string combining name and age on the browser.
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+// Create a variable called x.
+var x;
+// Output x on the browser.
+document.write(x);
+// An undefined operand is assigned to another variable x.
+var x = undefined;
+// Output x on the console.
+console.log(x);
