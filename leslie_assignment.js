4f2b699e4d2dbdcfee39213121a488f933dd4cce
Muneeza93 <65918723+Muneeza93@users.noreply.github.com>
2020-06-16 19:53:05 +0300

    Add files via upload

---
+++ leslie_assignment.js
@@ -0,0 +1,48 @@
+var name;
+//name is assigned the value 'ozzy queen'which is a string
+name = " ozzy queen"
+//printing the variable name 
+console.log(name);
+//insert a line break
+console.log("<br/>");
+//name is defined as a variable
+var name;
+//name is assigned the value 'ozzy hum'which is a string
+name = "ozzy hum"
+//printing the variable name 
+console.log(name)
+//name is defined as a variable
+var name;
+//name is assigned the value 'sumi mike ozyy'which is a string
+name = "sumi mike ozyy"
+//printing the variable name
+console.log(name)
+//name is defined as a variable
+var name;
+//name is assigned the value 'ozzy bp'which is a string 
+name = " ozzy bp"
+//printing the variable name
+console.log(name)
+//name is assigned the value 'Bridget ozzy'which is a string
+name = " Bridget ozzy"
+//printing the variable name
+console.log(name)
+//age is defined as a variable
+var age;
+//name is defined as a variable
+var name;
+//age is assigned the value 50 which is a number
+//name is assigned the value 'mirembe agaba kwesi'which is a string
+age = 50;
+name = "mirembe agaba kwesi"
+//write some text directly to the HTML document
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//x is defined as a variable
+var x;
+// write the value undefined to the HTML document
+document.write(x);
+//variable x has the value undefined 
+var x = undefined;
+//print the variable x
+console.log(x);
