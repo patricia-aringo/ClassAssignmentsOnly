a4e384a10c9487898191d491ebc1b3fe992774e8
HUMPHREY <65710028+Humphrey2016@users.noreply.github.com>
2020-06-16 19:18:23 +0300

    Add files via upload

---
+++ Humphrey.js
@@ -0,0 +1,48 @@
+// Declaring a variable called name 
+var name;
+// Assigning the string variable name to ozzy queen
+name = " ozzy queen"
+// A function which is used to print a variables called name
+console.log(name);
+//Creating a line break between variables or going to another variable
+console.log("<br/>");
+// Declaring a Variable called name
+var name;
+// Assigning the string variable name to ozzy hum
+name = "ozzy hum"
+// A function which is used to print a variables called name
+console.log(name)
+//Declaring the variable called name
+var name;
+//Assigning the string variable name to sumi mike ozyy
+name = "sumi mike ozyy"
+//A function which is used to print a variables called name
+console.log(name)
+//Declaring the variable called name
+var name;
+//Assigning the string variable name to ozzy bp
+name = " ozzy bp"
+//A function which is used to print a variables called name
+console.log(name)
+//Assigning another string variable name to Bridget ozzy 
+name = " Bridget ozzy"
+//A function which is used to print a variables called name
+console.log(name)
+// Declaring a variable called age
+var age;
+// Declaring a variable called name
+var name;
+//Assigning a numeric variable name called age to 50
+age = 50;
+//Assigning a string variable name to mirembe agaba kwesi
+name = "mirembe agaba kwesi"
+//HTML JS output used to publish content/code onto the browser but not the terminal
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+//Declaring a variable called x
+var x;
+// HTML JS output used to publish content/code onto the browser but not the terminal  
+document.write(x);
+//Assigning the variable name to undefined
+var x = undefined;
+//A function which is used to print a variables called x
+console.log(x);
