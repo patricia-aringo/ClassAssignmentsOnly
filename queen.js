53ca811e30769de29358e653327b6dfaa0cceed0
RefactoryCatalyst005 <66715088+RefactoryCatalyst005@users.noreply.github.com>
2020-06-16 14:35:11 +0300

    Add files via upload

---
+++ queen.js
@@ -0,0 +1,68 @@
+// declaring(creating) a global variable called 'name'.(giving name to aspace in a computer)
+var name;
+
+// assigning the 'name' variable a value of '0zzy queen' with = operator.
+name = " ozzy queen";
+
+//invoking  a function to print the value of name in the console.
+console.log(name);
+
+//printing in console <br/>.
+console.log("<br/>");
+
+//declaring(creating) a global scooped variable called 'name'.
+var name;
+
+//assigning the variable name with a value of ozzy hum.
+name = "ozzy hum";
+
+//calling a function to print(dispaly) ozzy hum in a console.
+console.log(name);
+
+//declaring avariable and giving it a name called 'name'.
+var name;
+
+//assigning a value of sumi mike ozyy to a variable called name.
+name = "sumi mike ozyy";
+
+// out putting the value of name in console.
+console.log(name)
+
+//declaring a variable and naming it.
+var name;
+
+//assigning a value of ozzy bp to a variable called name.
+name = " ozzy bp";
+
+//printing the value of name in the console by calling a console.log function.
+console.log(name);
+
+//assigning a value  to a variable.
+name = " Bridget ozzy";
+
+//invoking the funcion to print the value of name.                 
+console.log(name);
+
+//declaring and naming the age variable.
+var age;
+
+//declaring and naming the name variable.
+var name;
+
+// assigning the variables age and name  values.
+age = 50;
+name = "mirembe agaba kwesi";
+//this function is to print "my name is mirembe agaba kwesi and i am 50 years old in web browser.
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//declared the avariable named x but didnt assign any value.
+var x;
+
+//invoking a function to output the value of X. document.write is used in web browser.
+document.write(x);
+
+//the variable x is intialised with undefined value.
+var x = undefined;
+
+//invoking the function console.log()to print the the value x.
+console.log(x);
