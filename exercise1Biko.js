e46907c1f17f488a2830cffb731a430c6fc41f3e
Biko23 <Biko23@users.noreply.github.com>
2020-06-16 18:51:00 +0300

    BikozAssigment

---
+++ exercise1Biko.js
@@ -0,0 +1,52 @@
+
+
+
+ //Declaring a variable "name"
+var name;
+ //Initializing variable name with value "ozzy queen"
+name = " ozzy queen"
+ //printing out to the console the value of the variable name
+console.log(name);
+ // printing "<br/>"
+console.log("<br/>");
+ //Declaring a variable "name"
+var name;
+ //Initializing variable name with value "ozzy hum"
+name = "ozzy hum"
+ //printing out to the console the value of the variable name
+console.log(name)
+ //Declaring a variable "name"
+var name;
+ //Initializing variable name with value "sumi mike ozyy"
+name = "sumi mike ozyy"
+ //printing out to the console the value of the variable name
+console.log(name)
+ //Declaring a variable "name"
+var name;
+ //Initializing variable name with value "ozzy bp"
+name = " ozzy bp"
+ //printing out to the console the value of the variable name
+console.log(name)
+ //Initializing variable name with value "Bridget ozzy"
+name = " Bridget ozzy"
+ //printing out to the console the value of the variable name
+console.log(name)
+ //Declaring a variable "age"
+var age;
+ //Declaring a variable "name"
+var name;
+//Initializing variable name with value 50
+age = 50;
+//Initializing variable name with value "mirembe agaba kwesi"
+name = "mirembe agaba kwesi"
+/*Printing out "My name is
+mirembe agaba kwesi and I am 50 years old"*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+ //Declaring a variable "x"
+var x;
+ //printing out the value of the variable x which is undefined as it has not been initialized.
+document.write(x);
+ //Declaring a variable "x" and intializing it as undefined
+var x = undefined;
+ //printing out to the console the value of the variable x which is undefined
+console.log(x);
