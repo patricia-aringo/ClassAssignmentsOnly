5a4205fd50e8fc3a0525b8d2e0704e4e6de2b76e
Phil <ajeinstein3@gmail.com>
2020-06-16 18:43:17 +0300

    phillip.js

---
+++ phillip.js
@@ -0,0 +1,49 @@
+//var is used to declare variables.
+var name;
+//ozzy queen has been assigned to name.
+name = " ozzy queen"
+//console.log(name); prints 'ozzy queen' to our console.
+console.log(name);
+//console.log("<br/>"); loads a break point to our console.
+console.log("<br/>");
+//var is used to declare variables.
+var name;
+//'ozzy hum' has been assigned into name variable.
+name = "ozzy hum"
+//console.log(name); will print 'ozzy hum' in the console.
+console.log(name)
+//var is declaring name as a variable.
+var name;
+//'sumi mike ozyy' is assigned to name variable.
+name = "sumi mike ozyy"
+//console.log(name); will print 'sumi mike ozyy' in the console.
+console.log(name)
+//var is declaring name as a variable.
+var name;
+//'ozzy bp' has been assigned to name variable.
+name = " ozzy bp"
+//console.log(name); will print ' ozzy bp' in the console.
+console.log(name)
+//'Bridget ozzy' has been assigned to name variable.
+name = " Bridget ozzy"
+//console.log(name); will print 'Bridget ozzy' in our console.
+console.log(name)
+//var is declaring age as a variable
+var age;
+//var is declaring name as a variable.
+var name;
+//50 has been assigned to age variable while 'mirembe agaba kwesi'  has been assigned to name variable.
+age = 50;
+name = "mirembe agaba kwesi"
+//document.write will write the string concatenation "My name is " +name +" "+ "and I am "+age+" "+" years old" into a document in the console
+//The string will print 'My name is mirembe agaba kwesi and I am 50 years old'
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//var is declaring x as a variable.
+var x;
+//x in line 45 is an argument and the value in x will be printed in the console although x is undefined.
+document.write(x);
+//undefined has been assigned to x variable.
+var x = undefined;
+//The console will print undefined as the output for x.
+console.log(x);
