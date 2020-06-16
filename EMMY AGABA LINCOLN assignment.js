4a5437b7e7124e671008be36a487c897ae8f7069
Emmy Agaba <65983598+laemmy90@users.noreply.github.com>
2020-06-16 19:29:56 +0300

    Add files via upload

---
+++ EMMY AGABA LINCOLN assignment.js
@@ -0,0 +1,69 @@
+
+//declare variable name
+var name;
+
+//initialize variable name which stores string values
+name = " ozzy queen"
+
+//print out values assigned to variable name
+console.log(name);
+
+//prints <br/> that wasn't assigned a variable
+console.log("<br/>");
+
+//initialize variable name which stores string values
+name = "ozzy hum"
+
+//print outs value assigned to variable name
+console.log(name)
+
+//declare variable name 
+var name;
+
+//assign variable name vlaues
+name = "sumi mike ozyy"
+
+//print out variable name assigned a value
+console.log(name)
+
+//declare variable name
+var name;
+
+//assign value to variable name
+name = " ozzy bp"
+
+//print out variable
+console.log(name)
+
+//initialise value to variable name
+name = " Bridget ozzy"
+
+//prints out the value assigned to the value
+console.log(name)
+
+//declare variable name
+var age;
+
+//declare variable name
+var name;
+
+//assign variable age a value
+age = 50;
+
+//assign variable name a value
+name = "mirembe agaba kwesi"
+
+//line is used to express Javascript code to a document
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//initialise variable x
+var x;
+
+//express Javascript code to a document
+document.write(x);
+
+//variable was defined without a value
+var x = undefined;
+
+//prints line undefined
+console.log(x);
