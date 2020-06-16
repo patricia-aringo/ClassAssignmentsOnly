70b5e62ce65d607272c3fff27c319a01e60ca2c6
Kwesi Kutesa <65806184+kwesikutesa@users.noreply.github.com>
2020-06-16 19:04:42 +0300

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,50 @@
+//declare variable 'name'
+var name;
+
+// assign value 'ozzy queen' to variable name 'name'
+name = " ozzy queen"
+// display value of 'name' variable to console
+console.log(name);
+// display '<br/>' to console
+console.log("<br/>");
+// declare 'name' variable
+var name;
+// assign 'ozzy hum' to name
+name = "ozzy hum"
+// display value of 'name' variable to console
+console.log(name)
+// declare 'name' variable
+var name;
+// assign value 'sumi mike ozyy' to variable name 'name'
+name = "sumi mike ozyy"
+// display value of 'name' variable to console
+console.log(name)
+// declare 'name' variable
+var name;
+// assign value 'ozzy bp' to variable name 'name'
+name = " ozzy bp"
+// display value of 'name' variable to console
+console.log(name)
+// assign value 'Bridget ozzy' to variable name 'name'
+name = " Bridget ozzy"
+// display value of 'name' variable to console
+console.log(name)
+// declare 'age' variable
+var age;
+// declare 'name' variable
+var name;
+// assign value of 50 to variable name age
+age = 50;
+// assign value of 'mirembe agaba kwesi' to variable name 'name'
+name = "mirembe agaba kwesi"
+// write variable expression into an HTML output
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+
+var x;
+// write variable 'x' into an HTML output
+document.write(x);
+// assign x value as undefined
+var x = undefined;
+// display value of 'x' variable to console
+console.log(x);
