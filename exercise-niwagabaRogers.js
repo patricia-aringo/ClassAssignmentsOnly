d59ffee73d3cb9933e588778da41187b0f4e9773
niwagabaa <52739564+niwagabaa@users.noreply.github.com>
2020-06-16 21:24:27 +0300

    Exercise

---
+++ exercise-niwagabaRogers.js
@@ -0,0 +1,48 @@
+//Declaring the variable name
+var name;
+//Assigning the valuable to a string value
+name = " ozzy queen"
+//Printing the string values ozzy queen
+console.log(name);
+//the command is also going to print <br/>
+console.log("<br/>");
+//Declaring  a valuable name
+var name;
+//a new string is assigned to the value
+name = "ozzy hum"
+//this will print the string ozzy hum
+console.log(name)
+//Declaring a new valuable called name
+var name;
+//Assigning the string to the variable
+name = "sumi mike ozyy"
+//printing sumi mike ozyy
+console.log(name)
+// declaring a variable name
+var name;
+// Assigning the variable to a stirng value
+name = " ozzy bp"
+//printing ozzy bp
+console.log(name)
+//Assigning the a new value to the valuable; 
+name = " Bridget ozzy"
+// It will show the new value that has been assigned
+console.log(name)
+//Declaring a variable naming it age
+var age;
+//Declaring another variable called name
+var name;
+//Assigning the variables we created value number and string respectively
+age = 50;
+name = "mirembe agaba kwesi"
+//This is going to print the values assigned to name and age in the string typed
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//declaring a new variable x
+var x;
+//its going to bring an error since no value is assigned to x
+document.write(x);
+//the variable is assigned to undefined
+var x = undefined;
+//It is going to print undefined
+console.log(x);
