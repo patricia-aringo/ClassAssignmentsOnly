fe6baced7d0d208b6cbff5759739e5a83ee3fd23
renweiid <62768618+renweiid@users.noreply.github.com>
2020-06-16 19:17:03 +0300

    Add files via upload

---
+++ kakuba.js
@@ -0,0 +1,48 @@
+//Declaring of the variable name
+var name;
+//Variable name is assigned with a string "Ozzy queen"
+name = " ozzy queen"
+//instruction to print the value of name
+console.log(name);
+//instruction to print string "<br/>"
+console.log("<br/>");
+// variable name is declared
+var name;
+// string "Ozzy hum" is assigned to name
+name = "ozzy hum"
+// instruction to print the value of name
+console.log(name)
+// variable name is declared
+var name;
+// name is assigned with string "sumi mike ozyy"
+name = "sumi mike ozyy"
+// instruction to print the value of name
+console.log(name)
+// variable name is declared 
+var name;
+// variable name is assigned with string "ozzy bp"
+name = " ozzy bp"
+// instruction to print the value of the variable name 
+console.log(name)
+// variable name is initialized with string " Bridget ozzy"
+name = " Bridget ozzy"
+// instruction to print the value of variable name
+console.log(name)
+// varible age is declared
+var age;
+// variable name is declared 
+var name;
+// variable age is initialized with an integer 50, variable name is assigned with a string "mirembe agaba kwesi"
+age = 50;
+name = "mirembe agaba kwesi"
+// instruction to print the onto a page, the statement as displayed below but with the values of variables; name and age, concatenated therein in that order.
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+// variable x is declared
+var x;
+// instruction to print of x on a page
+document.write(x);
+// variable x is assigned with a data type undefined 
+var x = undefined;
+// instruction to print the value assigned to variable x on console
+console.log(x);
