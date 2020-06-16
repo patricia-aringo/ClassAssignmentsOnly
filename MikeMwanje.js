96f18d4f255750661e014a4dc7100bd6021a2e33
123MwanjeMike <41787587+123MwanjeMike@users.noreply.github.com>
2020-06-16 21:27:31 +0300

    Mike Mwanje Resubmission

---
+++ MikeMwanje.js
@@ -0,0 +1,70 @@
+// Declaring a variable called name
+var name;
+
+// Assigning the string value ( ozzy queen) to variable name
+name = " ozzy queen"
+
+// Printing the value( ozzy queen) in variable name to the console
+console.log(name);
+
+// Printing "<br/>" to the console
+console.log("<br/>");
+
+// Re-declaring the variable name
+var name;
+
+// Assigning the string value (ozzy hun) to variable name
+name = "ozzy hum"
+
+// Printing the new string value(ozzy hun) in the variable name to the console
+console.log(name)
+
+// Re-declaring the variable called name
+var name;
+
+// Assigning the new string value (sumi mike ozzy) to variable name
+name = "sumi mike ozyy"
+
+// Printing the new assigned string value(sumi mike ozzy) in the variable name to the console
+console.log(name)
+
+//Re-declaring the variable name
+var name;
+
+// Assigning the new string value ( ozzy bp) to variable name
+name = " ozzy bp"
+
+// Printing the new assigned string value in the variable name( ozzy bp) to the console
+console.log(name)
+
+// Assigning the string value ( Bridget ozzy) to the variable name
+name = " Bridget ozzy"
+
+// Printing the new assigned string value ( Bridget ozzy) to the console
+console.log(name)
+
+// Declaring a variable called age
+var age;
+
+// Re-declaring variable name
+var name;
+
+// Assigning number value 50 to variable age
+age = 50;
+// Assigning the variable name with a new string value (mirembe agaba kwesi)
+name = "mirembe agaba kwesi"
+
+// Writing ---My name is (mirembe agaba kwesi) and I am (50) years old--- to the HTML document
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+// Declaring a variable x
+var x;
+
+// Writing the value of variable x to the html document
+document.write(x);
+
+// Assigning undefined to variable x
+var x = undefined;
+
+// Printing the value in variable x to the console
+console.log(x);
