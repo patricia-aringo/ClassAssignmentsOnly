3101a808a7cd1ba449682b19d15228617789ed8c
sumaiya138 <65085150+sumaiya138@users.noreply.github.com>
2020-06-16 19:03:24 +0300

    uplaoded exercise

---
+++ tsumaiya_nabawanukaexercise.js
@@ -0,0 +1,47 @@
+var name;//A name valuable is declared and no value is assigned to it.
+
+name = " ozzy queen"// A string is assigned to the name valuable
+
+console.log(name); // the value of name is logged into the console.basically telling the cosole to display the value of name onto the screen
+
+console.log("<br/>"); // A new line is created because'<br/>' is a line braker
+
+var name;// Another valuable name is declared
+
+name = "ozzy hum".// And a string value is assigned to it which is ozzy hum
+
+console.log(name)// the valuable name is logged into the console to be printed
+
+var name;// Declaration of another valuable name
+
+name = "sumi mike ozyy"// A string value is assigned to the valuable
+
+console.log(name)// The valuable name is logged into the console to be out put on the screen
+
+var name;// Declaring a new valuable name
+
+name = " ozzy bp"//A name value is assigned to it
+
+console.log(name)// The name valuable is logged into the console
+
+name = " Bridget ozzy"// another name value is assigned to the name valuable
+
+console.log(name)// The name value is logged into the console to be printed
+
+var age;// Here we are declaring a new valuable which is age
+
+var name;// A name valuable is being declared
+
+age = 50;// The valuable age is being assigned a value to it which is 50
+name = "mirembe agaba kwesi"// The valuable name is being assigned a string value to it which is Mirembe agaba Kwesi
+
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");// write this exact text in the web browser
+
+
+var x;// we are declaring a valuable x
+
+document.write(x);// we are instrusting x to be written
+
+var x = undefined;// x is assigned as an undefined value
+
+console.log(x);// x is being logged into the console to be printed
