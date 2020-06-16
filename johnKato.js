fc9cecb2228044b4bf1404a359f62f46a641314f
batangaza <55735508+batangaza@users.noreply.github.com>
2020-06-16 19:10:05 +0300

    Kato's assignment, please do not delete :)

---
+++ johnKato.js
@@ -0,0 +1,68 @@
+// Declare a variable 'name'
+var name;
+
+// Assign the value 'ozzy queen' to name
+name = " ozzy queen"
+
+// print the value of name variable
+console.log(name);
+
+// log a break tag to the console
+console.log("<br/>");
+
+// Declare a variable "name"
+var name;
+
+// assign it the value, "ozzy hum"
+name = "ozzy hum"
+
+// print name
+console.log(name)
+
+
+// Declare a variable "name"
+var name;
+
+// assign it the value, "ozzy hum"
+name = "sumi mike ozyy"
+
+// print name
+console.log(name)
+
+// Declare a variable "name"
+var name;
+
+// assign it the value " ozzy bp"
+name = " ozzy bp"
+
+// print name
+console.log(name)
+
+// asssign it the value " Bridget ozzy"
+name = " Bridget ozzy"
+
+// out put value of name
+console.log(name)
+
+// Declare a variable 'age'
+var age;
+
+//Declare a variable 'name'
+var name;
+
+// assign var age a value 50 and name a value mirembe agaba kwesi
+age = 50;
+name = "mirembe agaba kwesi"
+// output different variable on the same line
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+// Declare a variable 'x'
+var x;
+
+// Output value of undefined variable x
+document.write(x);
+
+// Initialize X as undefined
+var x = undefined;
+// output x
+console.log(x);
