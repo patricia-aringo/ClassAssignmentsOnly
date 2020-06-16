c78d24cd5378c83bbfa8be71bd8f24850d59a090
EdBali <66214111+EdBali@users.noreply.github.com>
2020-06-16 19:40:50 +0300

    Eddie.js
    commented exercise

---
+++ Eddie.js
@@ -0,0 +1,53 @@
+//The variable ,'name'is declared
+var name;
+//The 'name' variable is aasigned a string value
+name = " ozzy queen"
+//The value of the name variable is printed
+console.log(name);
+//The text within the quotes is printed
+console.log("<br/>");
+/*(From  'name variable'- next comment):
+-The variable ,'name'is declared
+-The 'name' variable is aasigned a string value
+-The value of the name variable is printed
+The process is repeated after the third step is executed*/
+
+var name;
+
+name = "ozzy hum"
+
+console.log(name)
+
+var name;
+
+name = "sumi mike ozyy"
+
+console.log(name)
+
+var name;
+
+name = " ozzy bp"
+
+console.log(name)
+
+name = " Bridget ozzy"
+
+console.log(name)
+//two variables, 'age' and 'name' are declared
+var age;
+
+var name;
+//The variables are assigned values
+age = 50;
+name = "mirembe agaba kwesi"
+//The variables are printed in a statement
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//variable, 'x' is declared
+var x;
+//variable x is printed
+document.write(x);
+//The 'x' variable is assigned 'undefined'
+var x = undefined;
+//The 'x' variable is printed
+console.log(x);
