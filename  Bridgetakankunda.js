86d26585d1426ace3cd93509bc9e7fe18e290a5b
bridgetakankunda <64187124+bridgetakankunda@users.noreply.github.com>
2020-06-16 18:51:32 +0300

    Add files via upload

---
+++  Bridgetakankunda.js
@@ -0,0 +1,51 @@
+// Declaring a variable name
+var name;
+// Assigning a string to the variable name.
+name = " ozzy queen"
+// Logging the name variable to the console.
+console.log(name);
+// Line break.
+console.log("<br/>");
+// Declaring a variable.
+var name;
+// Assigning a string to the variable name.
+name = "ozzy hum"
+// Logging the name variable to the console.
+console.log(name)
+// Declaring a variable name.
+var name;
+// Assigning a string to the variable.
+name = "sumi mike ozyy"
+// logging the variable name to the console for the desired outcome.
+console.log(name)
+// Declaring a variable.
+var name;
+// Assigning a value to the variable.
+name = " ozzy bp"
+// Logging the variable name to the console.
+console.log(name)
+// Since the variable name was declared already,we assign it a value.
+name = " Bridget ozzy"
+// Logging the variable name to the console.
+console.log(name)
+
+var age;
+// Declaring the age variable.
+var name;
+// Declaring the name variable.
+age = 50;
+name = "mirembe agaba kwesi"
+// Assigning values to the variables above.
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+// Write is a command in javascript and + is an operator that combines values and strings.
+// The variable x was defined but not declared any value.
+var x;
+// Writing javascrit code in a document.
+document.write(x);
+// The value of x is undefined.
+var x = undefined;
+// The variable x is logged onto the console.
+console.log(x);
+
+
+
