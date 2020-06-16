9aba7f2763604e4d6dbd3338a9d451811b29849f
mirembeva <53422883+mirembeva@users.noreply.github.com>
2020-06-16 20:20:53 +0300

    Add files via upload

---
+++ Mirembe.js
@@ -0,0 +1,55 @@
+//Declaring a name variable in memory
+var name;
+//Assigning the string "Ozzy Queen" to the variable name as its value
+name = " ozzy queen"
+
+//Printing the value of name in the console
+console.log(name);
+// creating a new line
+console.log("<br/>");
+//Declaring a name variable in memory
+
+var name;
+//Assigning the string "Ozzy hum" to the variable name as its value
+
+name = "ozzy hum"
+//Printing the value of name in the console
+console.log(name)
+//Declaring a name variable in memory
+
+var name;
+//Assigning the string "sumi mike Ozzy" to the variable name as its value
+name = "sumi mike ozyy"
+//Printing the value of name in the console
+console.log(name)
+//Declaring a name variable in memory
+var name;
+//Assigning the string "Ozzy bp" to the variable name as its value
+name = " ozzy bp"
+//Printing the value of name in the console
+console.log(name)
+//Assigning the string "Bridget ozzy" to the variable name as its value
+name = " Bridget ozzy"
+//Printing the value of name in the console
+console.log(name)
+//Declaring variable age in memory
+var age;
+//Declaring a name variable in memory
+var name;
+//Assigning intializing the variabe age with an integer 50
+age = 50;
+//Assigning the string "mirembe agaba kwesi" to the variable name as its value
+name = "mirembe agaba kwesi"
+/*This will write into an html page the string "My name is " join it with the value of name for our case name is 
+the string "mirembe agaba kwesi"add a space " " Print the string "and I am " append it with the value of age, add a space and append a string " years old"
+string  "My name is mirembe agaba kwesi and I am 50 years old" will be written in an html document*/
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+//Declaring a variable x in memory
+var x;
+//Writing the value of x in an html page. for this case it is undefined
+document.write(x);
+//Assigning a data type undefined to the x variable
+var x = undefined;
+//Printing the value of x in the console
+console.log(x);
