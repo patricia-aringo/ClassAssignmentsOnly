75f930d65ad4c802f122dce2c14c5b18b4b47dec
Wabuluka <dwabuluka@gmail.com>
2020-06-16 14:22:03 +0300

    assignment

---
+++ wabuluka.js
@@ -0,0 +1,71 @@
+
+// variable declaration
+var name;
+
+// assignment of a variable
+name = " ozzy queen"
+
+// printing variable name to the console
+console.log(name);
+
+// printing <br/> on the console
+console.log("<br/>");
+
+// initializing a variable
+var name;
+
+// assinging variable name a value
+name = "ozzy hum"
+
+console.log(name)
+
+// initializing a variable
+var name;
+
+// assinging variable name a value
+name = "sumi mike ozyy"
+
+// printing variable name to the console
+console.log(name)
+
+// assinging variable name a value
+var name;
+
+// assinging variable name a value
+name = " ozzy bp"
+
+// printing variable name to the console
+console.log(name)
+
+// assinging variable name a value
+name = " Bridget ozzy"
+
+// printing variable name to the console
+console.log(name)
+
+// initializing a variable
+var age;
+
+// initializing a variable
+var name;
+
+// varable assignemnt
+age = 50;
+
+// variable assignment
+name = "mirembe agaba kwesi"
+
+// Write some text directly to the HTML document
+document.write("My name is " +name +" "+ "and I am "+age+" "+" years old");
+
+// crweating a variable
+var x;
+
+// Write some text directly to the HTML document which will return undefined
+document.write(x);
+
+// declaring variable x as undefined
+var x = undefined;
+
+// printing the undefined variable of x
+console.log(x);
