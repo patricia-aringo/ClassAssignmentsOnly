8fc1ac34160c70f8ab890dd0ccde5100bc6e1d8f
juliuswasajja-dig <65942820+juliuswasajja-dig@users.noreply.github.com>
2020-06-17 09:09:31 +0300

    Add files via upload
    Assignement 1 upload

---
+++ juliuswasajja.js
@@ -0,0 +1,49 @@
+var name;
+//a variable called name is declared
+name = " ozzy queen"
+    // the value "ozzy queen" is assigned to the name variable (initialisation)
+console.log(name);
+// the value "ozzy queen" of string data-type assigned to the name variable is printed on the screen through the terminal 
+console.log("<br/>");
+// the break html tag "<br/>" is printed through the terminal 
+var name;
+// variable called name is declared (variable declaration)
+name = "ozzy hum"
+    // the value "ozzy hum" of string data-type is assigned to the name variable (initialisation)
+console.log(name)
+    // the value "ozzy hum" assigned to the name variable is printed on the screen through the terminal 
+var name;
+// variable called name is declared
+name = "sumi mike ozyy"
+    //  the value "sumi mike ozyy" of string data-type is assigned to the name variable (initialisation)
+console.log(name)
+    // the value "sumi mike ozyy" assigned to the name variable is output on the screen through the terminal
+var name;
+// variable called name is declared/ variable declaration
+name = " ozzy bp"
+    // the value "ozzy bp" of string data-typeis assigned to the name variable (initialisation)
+console.log(name)
+    // the "ozzy bp" assigned to the name variable is printed on the screen through the terminal 
+name = " Bridget ozzy"
+    //the value "Bridget ozzy" of string data-type is assigned to the name variable
+console.log(name)
+    // the value "Bridget ozzy" assigned to the name variable is output on the screen through the terminal 
+var age;
+// variable called age is declared 
+var name;
+// variable called name is declared
+age = 50;
+name = "mirembe agaba kwesi"
+    // name varible is assigned a value "mirembe agaba kwesi" of string data-type and the age variable is assigned a value of "50" of int data-type
+document.write("My name is " + name + " " + "and I am " + age + " " + " years old");
+
+// an output message i.e  "My name is mirembe agaba kwesi and I am 50 years old" is to be seen through the web browser. NOTE: the variable "name" and "age" are being substituted with their values that is "mirembe agaba kwesi" and "50" respectively 
+// string concatenation is observed to ensure both strings and variables are printed out on the screen with in the same line of code/statement
+var x;
+// variable called x is declared 
+document.write(x);
+// "undefined" is output on the browser screen this is because variable x has not been assigned a value 
+var x = undefined;
+// variable x is assigned a value "undefined" of string data-type
+console.log(x);
+// the value "undefined" assigned to the name variable x is output on the screen through the terminal
