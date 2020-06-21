40bef85bb04b0d91654e0f2a9280309680cc3055
isaacssemugenyi <51030233+isaacssemugenyi@users.noreply.github.com>
2020-06-21 14:36:00 +0300

    Modified comments in code based on Ozzy's advice

--- isaac_ssemugenyi.js
+++ isaac_ssemugenyi.js
@@ -1,54 +1,50 @@
-//Declaring a variable called 'name'
+//Declaring a variable name
 var name;
-//Value " ozzy queen" is assigned to name variable
+//Assigning a value to the name variable
 name = " ozzy queen"
-    //The value in name variable is logged to the console
+//Outputting to the console the value of name
 console.log(name);
-//Logging a break html tag to the console
+//Outputting to the console an HTML br tag
 console.log("<br/>");
-//Re-declaring the variable 'name' but variable 'name' was already declared on line 2 above
+//Re-declaring the variable 'name'
 var name;
-//Re-assigning a value "ozzy hum" to variable name changing it from the previous value " ozzy queen"
+//Assigning a new value to the name variable
 name = "ozzy hum"
-    //Logging the value of variable name to the console which is "ozzy hum"
+//Outputting to the console the value of name
 console.log(name)
-    //Re-declaring the variable 'name' again
+//Re-declaring the variable 'name'
 var name;
-//Assigning variable 'name' with a value "sumi mike ozyy"
+//Assigning another new value to the name variable
 name = "sumi mike ozyy"
-    //Logging the value of variable name to the console which is now "sumi mike ozyy"
+//Outputting to the console the value of name
 console.log(name)
-    //Re-declaring the variable 'name' again
+//Re-declaring the variable 'name'
 var name;
-//Assigning variable 'name' with a value "ozzy bp"
+//Assigning another new value to the name variable
 name = " ozzy bp"
-    //Logging the value of variable 'name' to the console which is now " ozzy bp"
+//Outputting to the console the value of name
 console.log(name)
-    //Assigning the variable 'name' a value " Bridget ozzy", this changes the value of 'name' from " ozzy bp" to " Bridget ozzy"
+//Assigning another new value to the name variable
 name = " Bridget ozzy"
-    //Logging the value of variable 'name' to the console which is now " Bridget ozzy"
+//Outputting to the console the value of name
 console.log(name)
-    //Declaring a new variable 'age'
+//Declaring a variable 'age'
 var age;
 //Re-declaring the variable 'name' 
 var name;
-//Assigning the value of 50 to the 'age' variable and value "mirembe agaba kwesi" to the 'name' variable
+/**
+*Assigning a value to age variable
+*Assigning a value to name variable
+*/
 age = 50;
 name = "mirembe agaba kwesi"
-    /**
-     * We write to the client 'DOM' the statement 
-     * "My name is mirembe agaba kwesi and I am 50 years old"
-     * + name + we dynamically replace the 'name' variable with the value it is asigned to
-     * likewise for 'age'
-     * + " " + this creates a space in the statement
-     */
+//Output to the browser(Client)
 document.write("My name is " + name + " " + "and I am " + age + " " + " years old");
-
 //Declaring a variable 'x'
 var x;
-//Writing the value of 'x' variable to the client, which is 'undefined' since we have not assigned any value to the 'x' variable
+//Output to the browser(Client) the value x
 document.write(x);
-//A value of 'undefined' is assigned to the variable 'x'
+//Assigning 'undefined' to the variable 'x'
 var x = undefined;
-//Logging to the console the value of variable 'x' at the moment which is 'undefined'
+//Outputting to the console the value of x
 console.log(x);
