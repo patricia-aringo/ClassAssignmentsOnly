14d2f5ee8db9b633390b68dafe7112014122c469
Davis19094 <66644629+Davis19094@users.noreply.github.com>
2020-06-29 14:11:57 +0300

    Add files via upload

---
+++ mugume-classes.js
@@ -0,0 +1,112 @@
+
+var friend = {
+    name: "daniel",
+    color: "brown",
+    age: 24,
+    weight: 68,
+    height: 40,
+    phone_number: 0782752513,
+    university: "makerere",
+    love_life: () => {
+        console.log("am not sure")
+    }
+    origin: () => {
+        console.log(comes from buganda region)
+    }
+}
+
+var phone = {
+    type: "nokia",
+    ram: "4gb",
+    storage: "32gb",
+    color: "black",
+    durability: "5years",
+    camera: "16-megapics",
+    size: "17incg"
+    manufuctured: () => {
+        console.log("it comes from america")
+    }
+    company: () => {
+        console.log("microsoft")
+    }
+}
+
+var music = {
+    type: "reggea",
+    artist: "bob marly",
+    nationality: "jamaica"
+    age: "37years",
+    height: "42inch",
+    albums: 24
+    number_kids: "2kids"
+    death: () => {
+        console.log("died of cancer")
+    }
+    role: () => {
+        console.log("freedom fighter")
+    }
+}
+
+var radio = {
+    brand: "sony",
+    origin: "china",
+    size: "medium",
+    sound: "clear-sound",
+    owned: "villagers",
+    ports: "usb-port",
+    quality: "nice",
+    durability: () => {
+        console.log("it doesn't last for long")
+    }
+    produced: () => {
+        console.log("it was produced in 1940's")
+    }
+}
+
+var tv = {
+    name: "hisense",
+    size: "32inc",
+    color: "black",
+    waranty: "1 year",
+    weight: 14,
+    model:"2018-module",
+    sound:"clear-sound",
+    internet: () => {
+        console.log("it has installed internet")
+    }
+    storage: () => {
+        console.log("it has an inbuilt decoder")
+    }
+}
+
+var shoes = {
+    color: "brown",
+    material: "faux-leather",
+    weight: "1kg",
+    fastener: "zipper",
+    gender: "men",
+    type: "boot",
+    size: 32,
+    origin: = () => {
+        console.log("made in china")
+    }
+    description: = () => {
+        console.log("easy fitting,hard leather and durable")
+    }
+}
+
+var matress = {
+    model: "20 density standard",
+    name: "roseform",
+    color: "blue",
+    weight: "4kg",
+    material: "foam",
+    waranty: "1year",
+    size: "5by6",
+    durability: = () => {
+        console.log("it lasts for 15year")
+    }
+    price: = () => {
+        console.log("it costs 200,000")
+    }
+}
