67492433a29a0b6629cb97d1bafdf16b19d7e599
mirembeva <mirembeva@gmail.com>
2020-07-03 16:52:01 +0300

    mirembe

--- generalClasswork.js
+++ generalClasswork.js
@@ -1,1 +1,14 @@
+var validate=()=>{
 
+    var username=document.mydata.username;
+    var telephone=document.mydata.telephone;
+    var email=document.mydata.email; 
+
+var mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        if(email.value.match(mail)){
+            alert('correct email')
+        }else{
+            alert('wrong email address');
+            email.style.border='2px solid red';
+        }
+    }
